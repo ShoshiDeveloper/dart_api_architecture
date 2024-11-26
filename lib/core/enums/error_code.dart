e647d3d8571e9ea187173eb4fbc7e79f43b710de
shoshideveloper <lobachev-2019@mail.ru>
2024-11-26 21:49:15 +0500

    refactoring error codes

--- lib/core/enums/error_code.dart
+++ lib/core/enums/error_code.dart
@@ -4,28 +4,32 @@ import 'package:supabase/supabase.dart';
 
 /**
  * U - User
+ * C - Client
  * S - Server
  * SP - Server - Postgrest
- * CJ - Client
+ * SSMTP - Server - SMTP
  */
 
 enum ErrorCodes {
   //User error codes
-  unauthorized(httpCode: 401, code: 'U1', message: 'Unauthorized'),
-  accessDenied(httpCode: 403, code: 'U3', message: 'Access denied'),
-
-  //Server error codes
-  undefiend(httpCode: 500, code: 'S0', message: 'Something went wrong'),
-  notImplemented(httpCode: 501, code: 'S1', message: 'Not implemented'),
-  unvailable(httpCode: 503, code: 'S3', message: 'The service is temporarily unavailable'),
-
-  //Postgrest error codes
-  postgress(httpCode: 500, code: 'SP1', message: 'FATAL SERVER ERROR'),
+  unauthorized(httpCode: 401, code: 'U0', message: 'Unauthorized'),
+  accessDenied(httpCode: 403, code: 'U1', message: 'Access denied'),
 
   //Client error codes
-  wrongJson(httpCode: 500, code: 'C1', message: 'Received wrong JSON'),
-  brokenJson(httpCode: 500, code: 'C2', message: 'Received broken JSON'),
-  alreadyExists(httpCode: 409, code: 'C3', message: 'This is already exists');
+  wrongJson(httpCode: 400, code: 'C0', message: 'Received wrong JSON'),
+  brokenJson(httpCode: 406, code: 'C1', message: 'Received broken JSON'),
+  alreadyExists(httpCode: 409, code: 'C2', message: 'This is already exists'),
+
+  //Server error codes
+  undefiend(httpCode: 520, code: 'S0', message: 'Something went wrong'),
+  notImplemented(httpCode: 501, code: 'S1', message: 'Not implemented'),
+  unvailable(httpCode: 503, code: 'S2', message: 'The service is temporarily unavailable'),
+
+  //Postgrest error codes
+  postgress(httpCode: 500, code: 'SP0', message: 'Database error'),
+
+  //SMTP error codes
+  smtp(httpCode: 500, code: 'SSMTPP0', message: 'Error sending email');
 
   const ErrorCodes({required this.httpCode, required this.code, required this.message});
 
