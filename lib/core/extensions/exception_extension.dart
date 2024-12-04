c73b8ead7e0eed056da9f3ec4cd3d0f4aac12a31
Pavel-Notebook <lobachev-2019@mail.ru>
2024-12-04 16:32:12 +0500

    refactoring exception_extension

--- lib/core/extensions/exception_extension.dart
+++ lib/core/extensions/exception_extension.dart
@@ -6,16 +6,15 @@ import 'package:dart_frog/dart_frog.dart';
 extension ExceptionExtension on Exception {
   Response getRresponseWithTrace(StackTrace st) {
     Logger.instance.e(this, st: st);
-    if (this is RepositoryException) {
-      return (this as RepositoryException).errorCode.response;
-    }
-
-    final errorCode = ErrorCodes.byException(this);
-    return errorCode.response;
+    return _response;
   }
 
   Response get response {
     Logger.instance.e(this);
+    return _response;
+  }
+
+  Response get _response {
     if (this is RepositoryException) {
       return (this as RepositoryException).errorCode.response;
     }
